1b93ffb2cc6c899ba36e9983cf8047092ea139b9
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2023-06-23 20:20:03 +0300

    Blocked-swipe viewPager

--- app/src/main/java/good/damn/scriptengine/activities/MainActivity.java
+++ app/src/main/java/good/damn/scriptengine/activities/MainActivity.java
@@ -1,28 +1,32 @@
 package good.damn.scriptengine.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-import good.damn.scriptengine.adapters.FragmentPagerAdapter;
 import good.damn.scriptengine.fragments.PiecesListFragment;
 import good.damn.scriptengine.fragments.ResourcesFragment;
 import good.damn.scriptengine.fragments.ScriptEditorFragment;
 import good.damn.scriptengine.interfaces.OnClickTextPiece;
 import good.damn.scriptengine.models.Piece;
+import good.damn.scriptengine.views.BlockedViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private ViewPager2 mViewPager;
+    private BlockedViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mViewPager = new ViewPager2(this);
+        mViewPager = new BlockedViewPager(this);
+        mViewPager.setId(ViewCompat.generateViewId());
 
         mViewPager.setOffscreenPageLimit(3);
 
@@ -40,12 +44,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mViewPager.setAdapter(new FragmentPagerAdapter(this, new Fragment[]{
+        final Fragment[] fragments = new Fragment[]{
                 piecesListFragment,
                 scriptEditorFragment,
                 resourcesFragment
-        }));
+        };
 
+        mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @NonNull
+            @Override
+            public Fragment getItem(int position) {
+                return fragments[position];
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.length;
+            }
+        });
         setContentView(mViewPager);
     }
 
