754a42612bcad54e98be64bd48f5da13a86368d7
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2023-07-06 17:12:38 +0300

    Full screen mode for PreviewActivity

--- app/src/main/java/good/damn/scriptengine/activities/PreviewActivity.java
+++ app/src/main/java/good/damn/scriptengine/activities/PreviewActivity.java
@@ -226,14 +226,16 @@ public class PreviewActivity extends AppCompatActivity {
                     }
                 });
 
-                traceView.setAlpha(0);
-
                 root_FrameLayout.addView(traceView,
                         FrameLayout.LayoutParams.MATCH_PARENT,
                         FrameLayout.LayoutParams.MATCH_PARENT);
 
+                traceView.setAlpha(0);
+
+
                 traceView.animate()
                         .alpha(1.0f)
+                        .setDuration(1650)
                         .start();
             }
 
@@ -270,6 +272,7 @@ public class PreviewActivity extends AppCompatActivity {
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_UP:
+
                         mColorRevealView.setCenterPoint(motionEvent.getX(),
                                 motionEvent.getY());
                         if (mCurrentViewPhrase != null) {
@@ -303,5 +306,21 @@ public class PreviewActivity extends AppCompatActivity {
 
         mSFXPool.autoPause();
         mSFXPool.release();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                    View.SYSTEM_UI_FLAG_IMMERSIVE |
+                                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    );
+        }
     }
 }
